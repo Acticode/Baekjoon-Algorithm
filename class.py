ff747f5fd220fabfaae3cca28104417cb04540e7
Biny <min.tiy1014@gmail.com>
2025-12-12 23:19:09 +0900

    15829

--- class.py
+++ class.py
@@ -1,11 +1,16 @@
-def cal(num, nl) :
-	for i in nl :
-		num -= i
-	print(num)
+def cal(nl) :
+	h = 0
+	for i in range(len(nl)) :
+		h += ( ord(nl[i]) - ord('a') + 1 )  * (31 ** i)
+	if h >= 1234567891 :
+		h %= 1234567891
+	print(h)
 
 
 def main() :
+	input()
 	x = input()
-	y = list(map(int, x.split()))
-	cal(x, y)
-	
+	cal(x)
+
+
+main()
