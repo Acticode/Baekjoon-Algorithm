cc9f95c17f321d64779181825d9a3819c21508ab
Biny <min.tiy1014@gmail.com>
2025-12-13 23:00:27 +0900

    2869

--- class.py
+++ class.py
@@ -1,21 +1,16 @@
-def cal(f, r) :
-	l = [ ([0] * r) for _ in range(f+1) ]
-	for i in range(r) :
-		l[0][i] = i+1
-	for j in range(1, f+1) :
-		l[j][0] = l[j-1][0]
-		for k in range(r) :
-			l[j][k] = l[j][k-1] + l[j-1][k]
-	print(l[f][r-1])
+def cal(a, b, v) :
 
+	if v < a :
+		return 1
+
+	numer = v - a
+	denom = a - b
+	return (numer + denom - 1) // denom + 1
 
 
 def main() :
-	x = int(input())
-	for i in range(x) :
-		floor = int(input())
-		room = int(input())
-		cal(floor, room)
+	x = list(map(int, input().split()))
+	print(cal(x[0], x[1], x[2]))
 
 
 main()
