cd98078a5d81560e739d2ef3635949892dc9c96d
Biny <min.tiy1014@gmail.com>
2025-12-14 15:00:32 +0900

    10989

--- class.py
+++ class.py
@@ -1,16 +1,29 @@
-def cal(a, b, v) :
-
-	if v < a :
-		return 1
-
-	numer = v - a
-	denom = a - b
-	return (numer + denom - 1) // denom + 1
+import sys
 
 
 def main() :
-	x = list(map(int, input().split()))
-	print(cal(x[0], x[1], x[2]))
+	try:
+		n = int(sys.stdin.readline())
+	except:
+		return
+
+	l = [0] * 10001
+
+	for _ in range(n) :
+		try:
+			x = int(sys.stdin.readline())
+			l[x] += 1
+		except :
+			continue
+
+
+	for i in range(1, 10001) :
+		if l[i] > 0 :
+			for _ in range(l[i]) :
+				print(i)
+
+
+
 
 
 main()
